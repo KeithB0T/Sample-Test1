9812f4f4a3a97a97f027a574eadf31547d719e5a
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-08-15 11:25:56 -0400

    Add project files.

---
+++ Sample-Test1/student.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+	cout << (a + b)<<endl;
+	return a + b;
+}
+
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "student.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(add(3,4), 7);
+  EXPECT_TRUE(true);
+}
